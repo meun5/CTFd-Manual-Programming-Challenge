6a9c6d176bf2eba41590e425b2374248de96490a
Alexander Young <stdin@iastate.edu>
2020-05-11 23:54:23 -0500

    Add models and Challenge methods

--- __init__.py
+++ __init__.py
@@ -30,6 +30,139 @@ class ManualGradingChallenge(BaseChallenge):
     # Blueprint used to access the static_folder directory.
     blueprint = manual
 
+    @staticmethod
+    def create(request):
+        data = request.form or request.get_json()
+
+        challenge = ManualChallenge(**data)
+
+        db.session.add(challenge)
+        db.session.commit()
+
+        return challenge
+
+    @staticmethod
+    def read(challenge):
+        """
+        This method is in used to access the data of a challenge in a format processable by the front end.
+
+        :param challenge:
+        :return: Challenge object, data dictionary to be returned to the user
+        """
+        challenge = ManualChallenge.query.filter_by(id=challenge.id).first()
+        data = {
+            "id": challenge.id,
+            "name": challenge.name,
+            "value": challenge.value,
+            "initial": challenge.initial,
+            "description": challenge.description,
+            "category": challenge.category,
+            "state": challenge.state,
+            "max_attempts": challenge.max_attempts,
+            "type": challenge.type,
+            "type_data": {
+                "id": ManualGradingChallenge.id,
+                "name": ManualGradingChallenge.name,
+                "templates": ManualGradingChallenge.templates,
+                "scripts": ManualGradingChallenge.scripts,
+            },
+        }
+        return data
+
+    @staticmethod
+    def update(challenge, request):
+        """
+        This method is used to update the information associated with a challenge. This should be kept strictly to the
+        Challenges table and any child tables.
+
+        :param challenge:
+        :param request:
+        :return:
+        """
+        data = request.form or request.get_json()
+
+        for attr, value in data.items():
+            setattr(challenge, attr, value)
+
+        db.session.commit()
+
+        return challenge
+
+    @staticmethod
+    def attempt(challenge, request):
+        """
+        This method is used to check whether a given input is right or wrong. It does not make any changes and should
+        return a boolean for correctness and a string to be shown to the user. It is also in charge of parsing the
+        user's input from the request itself.
+
+        :param challenge: The Challenge object from the database
+        :param request: The request the user submitted
+        :return: (boolean, string)
+        """
+
+        return False, "Pending"
+
+    @staticmethod
+    def solve(user, team, challenge, request):
+        """
+        This method is used to insert Solves into the database in order to mark a challenge as solved.
+
+        :param team: The Team object from the database
+        :param chal: The Challenge object from the database
+        :param request: The request the user submitted
+        :return:
+        """
+        data = request.form or request.get_json()
+        submission = data["submission"].strip()
+        solve = Solves(
+            user_id=user.id,
+            team_id=team.id if team else None,
+            challenge_id=challenge.id,
+            ip=get_ip(req=request),
+            provided=submission,
+        )
+        db.session.add(solve)
+        db.session.commit()
+        db.session.close()
+
+    @staticmethod
+    def fail(user, team, challenge, request):
+        """
+        This method is used to insert Fails into the database in order to mark an answer incorrect.
+
+        :param team: The Team object from the database
+        :param chal: The Challenge object from the database
+        :param request: The request the user submitted
+        :return:
+        """
+        data = request.form or request.get_json()
+        submission = data["submission"].strip()
+        wrong = Pending(
+            user_id=user.id,
+            team_id=team.id if team else None,
+            challenge_id=challenge.id,
+            ip=get_ip(request),
+            provided=submission,
+        )
+        db.session.add(wrong)
+        db.session.commit()
+        db.session.close()
+
+
+class ManualChallenge(Challenges):
+    __mapper_args__ = {"polymorphic_identity": "manual"}
+    id = db.Column(
+        db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True
+    )
+    initial = db.Column(db.Integer, default=0)
+
+    def __init__(self, *args, **kwargs):
+        super(ManualChallenge, self).__init__(**kwargs)
+        self.initial = kwargs["value"]
+
+
+class Pending(Submissions):
+    __mapper_args__ = {"polymorphic_identity": "pending"}
 
 
 def load(app):
